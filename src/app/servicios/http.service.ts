40cd87e6c8708eedd4c3bec80d529d5ff454ad91
patoattie <patoattie@gmail.com>
2020-05-14 21:38:05 -0300

    parcial 1

--- src/app/servicios/http.service.ts
+++ src/app/servicios/http.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -12,15 +12,8 @@ export class HttpService {
   ) { }
 
   public peticionGet(url: string, params: any): Observable<any> {
-// console.log(url);
     const paramsHttp = {params};
-    /*const paramsHttp = new HttpParams();
-
-    paramsHttp.append('api_key', '73f9841'.concat('cbba42072').concat('a2e12038').concat('92c07179'));
-    paramsHttp.append('sort_by', 'popularity.desc');
-    paramsHttp.append('language', 'es-MX');*/
 
     return this.http.get(url, paramsHttp);
-    // return this.http.get(url, {params: {api_key: '73f9841'.concat('cbba42072').concat('a2e12038').concat('92c07179'), sort_by: 'popularity.desc', language: 'es-MX'}});
   }
 }
